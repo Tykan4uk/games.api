77fe6c1dd4066aee91b5510eeb6737e35096e7f9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-06-27 16:41:21 +0300

    Startup: addings services and config

--- GamesApi/src/GamesApi/Startup.cs
+++ GamesApi/src/GamesApi/Startup.cs
@@ -1,27 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GamesApi.Configuration;
+using GamesApi.Data;
+using GamesApi.DataProviders;
+using GamesApi.DataProviders.Abstractions;
+using GamesApi.Services;
+using GamesApi.Services.Abstractions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace GamesApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup()
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            AppConfiguration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration AppConfiguration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +35,12 @@ namespace GamesApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GamesApi", Version = "v1" });
             });
+            services.Configure<Config>(AppConfiguration);
+            var connectionString = AppConfiguration["BookApi:ConnectionString"];
+            services.AddDbContext<GamesDbContext>(
+                opts => opts.UseNpgsql(connectionString));
+            services.AddTransient<IGameProvider, GameProvider>();
+            services.AddTransient<IGameService, GameService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
