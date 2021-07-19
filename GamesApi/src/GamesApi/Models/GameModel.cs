90ca154c7e2a3725fc9eb7c073b3dde4b79f059b
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 21:56:49 +0300

    Adding GameModel

---
+++ GamesApi/src/GamesApi/Models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GamesApi.Models
+{
+    public class GameModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Developer { get; set; }
+        public string Publisher { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
