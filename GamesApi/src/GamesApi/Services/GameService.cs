3252f93fc48bb1d7381fab9b2c291e0100fb8278
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 00:36:40 +0300

    GameService: now all method get partial data and return responses. Adding new Update methods for all Game`s property.

--- GamesApi/src/GamesApi/Services/GameService.cs
+++ GamesApi/src/GamesApi/Services/GameService.cs
@@ -1,48 +1,132 @@
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using GamesApi.Data;
 using GamesApi.DataProviders.Abstractions;
+using GamesApi.Models;
+using GamesApi.Models.Responses;
 using GamesApi.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamesApi.Services
 {
-    public class GameService : IGameService
+    public class GameService : BaseDataService, IGameService
     {
         private readonly IGameProvider _gameProvider;
+        private readonly IMapper _mapper;
 
-        public GameService(IGameProvider gameProvider)
+        public GameService(
+            IDbContextFactory<GamesDbContext> factory,
+            IGameProvider gameProvider,
+            IMapper mapper)
+            : base(factory)
         {
             _gameProvider = gameProvider;
+            _mapper = mapper;
         }
 
-        public async Task<IEnumerable<GameEntity>> GetByPageAsync(int page)
+        public async Task<GetByPageResponse> GetByPageAsync(int page, int pageSize)
         {
-            return await _gameProvider.GetByPageAsync(page);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.GetByPageAsync(page, pageSize);
+
+                return _mapper.Map<GetByPageResponse>(result);
+            });
         }
 
-        public async Task<GameEntity> GetByIdAsync(int id)
+        public async Task<GetByIdResponse> GetByIdAsync(string id)
         {
-            return await _gameProvider.GetByIdAsync(id);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.GetByIdAsync(id);
+
+                var game = _mapper.Map<GameModel>(result);
+
+                return new GetByIdResponse() { Game = game };
+            });
         }
 
-        public async Task<IEnumerable<GameEntity>> GetListGameByListIdAsync(HashSet<int> listId)
+        public async Task<AddResponse> AddAsync(string name, string developer, string publisher, string genre, DateTime releaseDate, decimal price)
         {
-            return await _gameProvider.GetListGameByListIdAsync(listId);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.AddAsync(name, developer, publisher, genre, releaseDate, price);
+
+                var game = _mapper.Map<GameModel>(result);
+
+                return new AddResponse() { Game = game };
+            });
         }
 
-        public async Task<GameEntity> AddAsync(GameEntity game)
+        public async Task<DeleteResponse> DeleteAsync(string id)
         {
-            return await _gameProvider.AddAsync(game);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.DeleteAsync(id);
+
+                return new DeleteResponse() { IsDeleted = result };
+            });
         }
 
-        public async Task<bool> DeleteAsync(int id)
+        public async Task<UpdateResponse> UpdateNameAsync(string id, string name)
         {
-            return await _gameProvider.DeleteAsync(id);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdateNameAsync(id, name);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
         }
 
-        public async Task<bool> UpdateAsync(GameEntity game)
+        public async Task<UpdateResponse> UpdateDeveloperAsync(string id, string developer)
         {
-            return await _gameProvider.UpdateAsync(game);
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdateDeveloperAsync(id, developer);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
+        }
+
+        public async Task<UpdateResponse> UpdatePublisherAsync(string id, string publisher)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdatePublisherAsync(id, publisher);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
+        }
+
+        public async Task<UpdateResponse> UpdateGenreAsync(string id, string genre)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdateGenreAsync(id, genre);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
+        }
+
+        public async Task<UpdateResponse> UpdateReleaseDateAsync(string id, DateTime releaseDate)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdateReleaseDateAsync(id, releaseDate);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
+        }
+
+        public async Task<UpdateResponse> UpdatePriceAsync(string id, decimal price)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                var result = await _gameProvider.UpdatePriceAsync(id, price);
+
+                return new UpdateResponse() { IsUpdated = result };
+            });
         }
     }
 }
