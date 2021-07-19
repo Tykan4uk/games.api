e5a371f9cf59c13591598a81cf2240855e8b1685
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 21:27:49 +0300

    GameEntity: change type of Id int=>string(aka guid) and transfer to Data/Entities (change namespace too)

--- GamesApi/src/GamesApi/Data/Entities/GameEntity.cs
+++ GamesApi/src/GamesApi/Data/Entities/GameEntity.cs
@@ -1,10 +1,10 @@
 using System;
 
-namespace GamesApi.Data
+namespace GamesApi.Data.Entities
 {
     public class GameEntity
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Developer { get; set; }
         public string Publisher { get; set; }
