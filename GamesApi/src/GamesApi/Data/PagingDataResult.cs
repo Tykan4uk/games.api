a2484eab0d330d97d3bdf3f31c983c7f3861724e
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 22:17:31 +0300

    Adding PagingDataResult

---
+++ GamesApi/src/GamesApi/Data/PagingDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GamesApi.Data.Entities;
+
+namespace GamesApi.Data
+{
+    public class PagingDataResult
+    {
+        public IEnumerable<GameEntity> Games { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
