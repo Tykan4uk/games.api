a945350862f11ada19d9ece9218d906ae394bc83
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 22:17:41 +0300

    Adding MappingProfile

---
+++ GamesApi/src/GamesApi/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GamesApi.Data;
+using GamesApi.Data.Entities;
+using GamesApi.Models;
+using GamesApi.Models.Responses;
+
+namespace GamesApi.Common.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<GameModel, GameEntity>().ReverseMap();
+            CreateMap<GetByPageResponse, PagingDataResult>().ReverseMap();
+        }
+    }
+}
