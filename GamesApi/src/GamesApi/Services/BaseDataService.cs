d52a38e6cdc3051fa4b6ea4c19f629b83b4e499f
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 21:48:59 +0300

    Adding class-wrap BaseDataService

---
+++ GamesApi/src/GamesApi/Services/BaseDataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using GamesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesApi.Services
+{
+    public class BaseDataService
+    {
+        private readonly GamesDbContext _gamesDbContext;
+
+        public BaseDataService(IDbContextFactory<GamesDbContext> dbContextFactory)
+        {
+            _gamesDbContext = dbContextFactory.CreateDbContext();
+        }
+
+        protected async Task<T> ExecuteSafe<T>(Func<Task<T>> action)
+        {
+            using (var transaction = _gamesDbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = await action();
+                    transaction.Commit();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
