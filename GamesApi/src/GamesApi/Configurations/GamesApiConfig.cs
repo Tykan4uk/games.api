c7856df457dc7a9c036901679ffbdc4a537b59ba
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 21:17:39 +0300

    GamesApiConfig: deleting PageSize. PageSize now can get from front.

--- GamesApi/src/GamesApi/Configurations/GamesApiConfig.cs
+++ GamesApi/src/GamesApi/Configurations/GamesApiConfig.cs
@@ -3,6 +3,5 @@ namespace GamesApi.Configuration
     public class GamesApiConfig
     {
         public string ConnectionString { get; set; } = null!;
-        public int PageSize { get; set; }
     }
 }
