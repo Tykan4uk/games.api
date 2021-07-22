1e28d71ce87a0f03e141595355844ef3f4b9887c
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-22 10:41:14 +0300

    GameServiceTest: adding some tests

--- GamesApi/tests/GamesApi.UnitTests/Services/GameServiceTest.cs
+++ GamesApi/tests/GamesApi.UnitTests/Services/GameServiceTest.cs
@@ -1,11 +1,17 @@
-﻿using FluentAssertions;
+﻿using AutoMapper;
+using FluentAssertions;
 using GamesApi.Data;
+using GamesApi.Data.Entities;
 using GamesApi.DataProviders.Abstractions;
+using GamesApi.Models;
+using GamesApi.Models.Responses;
 using GamesApi.Services;
 using GamesApi.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace GamesApi.UnitTests.Services
@@ -15,204 +21,73 @@ namespace GamesApi.UnitTests.Services
         private readonly IGameService _gameService;
 
         private readonly Mock<IGameProvider> _gameProvider;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IDbContextFactory<GamesDbContext>> _dbContextFactory;
 
-        private readonly GameEntity _gameTest;
-
-        private List<GameEntity> _gameListTest;
+        private readonly GameEntity _gameEntitySuccess = new GameEntity()
+        {
+            Name = "TestName"
+        };
+        private readonly GameModel _gameModelSuccess = new GameModel()
+        {
+            Name = "TestName"
+        };
+        private readonly GameEntity _gameEntityFailed = new GameEntity()
+        {
+        };
+        private readonly GameModel _gameModelFailed = new GameModel()
+        {
+        };
+        private readonly string _testIdSuccess = "testIdSuccess";
+        private readonly string _testIdFail = "testIdFail";
 
         public GameServiceTest()
         {
-            _gameTest = new GameEntity()
-            {
-                Id = 0,
-                Name = "Name",
-                Developer = "Developer",
-                Publisher = "Publisher",
-                Genre = "Genre",
-                ReleaseDate = DateTime.Now,
-                Price = 1
-            };
-
-            _gameListTest = new List<GameEntity>()
-            {
-                _gameTest
-            };
-
             _gameProvider = new Mock<IGameProvider>();
-
-            _gameProvider.Setup(s => s.GetByPageAsync(
-                It.IsAny<int>())).ReturnsAsync(_gameListTest);
-
-            _gameProvider.Setup(s => s.GetByPageAsync(
-                It.IsInRange<int>(-10, -1, Moq.Range.Inclusive))).ReturnsAsync(new List<GameEntity>());
+            _mapper = new Mock<IMapper>();
+            _dbContextFactory = new Mock<IDbContextFactory<GamesDbContext>>();
 
             _gameProvider.Setup(s => s.GetByIdAsync(
-                It.IsAny<int>())).ReturnsAsync(_gameTest);
+                It.Is<string>(i => i.Contains(_testIdSuccess)))).ReturnsAsync(_gameEntitySuccess);
 
             _gameProvider.Setup(s => s.GetByIdAsync(
-                It.IsInRange<int>(-10, -1, Moq.Range.Inclusive))).ReturnsAsync(new GameEntity());
+                It.Is<string>(i => i.Contains(_testIdFail)))).ReturnsAsync(_gameEntityFailed);
 
-            _gameProvider.Setup(s => s.GetListGameByListIdAsync(
-                It.IsNotNull<HashSet<int>>())).ReturnsAsync(_gameListTest);
+            _mapper.Setup(s => s.Map<GameModel>(
+                It.Is<GameEntity>(i => i.Equals(_gameEntitySuccess)))).Returns(_gameModelSuccess);
 
-            _gameProvider.Setup(s => s.AddAsync(
-                It.IsNotNull<GameEntity>())).ReturnsAsync(_gameTest);
+            _mapper.Setup(s => s.Map<GameModel>(
+                It.Is<GameEntity>(i => i.Equals(_gameEntityFailed)))).Returns(_gameModelFailed);
 
-            _gameProvider.Setup(s => s.DeleteAsync(
-                It.IsAny<int>())).ReturnsAsync(true);
-
-            _gameProvider.Setup(s => s.DeleteAsync(
-                It.IsInRange<int>(-10, -1, Moq.Range.Inclusive))).ReturnsAsync(false);
-
-            _gameProvider.Setup(s => s.UpdateAsync(
-                It.IsNotNull<GameEntity>())).ReturnsAsync(true);
-
-            _gameService = new GameService(_gameProvider.Object);
+            _gameService = new GameService(_dbContextFactory.Object, _gameProvider.Object, _mapper.Object);
         }
 
         [Fact]
-        public async void GetByPageAsyncTestSuccess()
-        {
-            //arrange
-            var pageTest = 0;
-
-            //act
-            var result = await _gameService.GetByPageAsync(pageTest);
-
-            //assert
-            result.Should().BeSameAs(_gameListTest);
-        }
-        [Fact]
-        public async void GetByPageAsyncTestFailed()
-        {
-            //arrange
-            var pageTest = -1;
-
-            //act
-            var result = await _gameService.GetByPageAsync(pageTest);
-
-            //assert
-            result.Should().NotBeSameAs(_gameListTest);
-        }
-
-        [Fact]
-        public async void GetByIdAsyncTestSuccess()
-        {
-            //arrange
-            var idTest = 0;
-
-            //act
-            var result = await _gameService.GetByIdAsync(idTest);
-
-            //assert
-            result.Should().BeSameAs(_gameTest);
-        }
-        [Fact]
-        public async void GetByIdAsyncTestFailed()
-        {
-            //arrange
-            var idTest = -1;
-
-            //act
-            var result = await _gameService.GetByIdAsync(idTest);
-
-            //assert
-            result.Should().NotBeSameAs(_gameTest);
-        }
-
-        [Fact]
-        public async void GetListGameByListIdAsyncTestSuccess()
-        {
-            //arrange
-            var idListTest = new HashSet<int>() { 0 };
-
-            //act
-            var result = await _gameService.GetListGameByListIdAsync(idListTest);
-
-            //assert
-            result.Should().BeSameAs(_gameListTest);
-        }
-        [Fact]
-        public async void GetListGameByListIdAsyncTestFailed()
+        public async Task GetByIdAsync_Success()
         {
             //arrange
 
             //act
-            var result = await _gameService.GetListGameByListIdAsync(null);
+            var result = await _gameService.GetByIdAsync(_testIdSuccess);
 
             //assert
-            result.Should().BeEmpty();
+            result.Should().NotBeNull();
+            result.Game.Should().NotBeNull();
+            result.Game.Name.Should().Be("TestName");
         }
 
         [Fact]
-        public async void AddAsyncTestSuccess()
+        public async Task GetByIdAsync_Failed()
         {
             //arrange
 
             //act
-            var result = await _gameService.AddAsync(_gameTest);
+            var result = await _gameService.GetByIdAsync(_testIdFail);
 
             //assert
-            result.Should().BeSameAs(_gameTest);
-        }
-        [Fact]
-        public async void AddAsyncTestFailed()
-        {
-            //arrange
-
-            //act
-            var result = await _gameService.AddAsync(null);
-
-            //assert
-            result.Should().BeNull();
-        }
-
-        [Fact]
-        public async void DeleteAsyncTestSuccess()
-        {
-            //arrange
-            var idTest = 0;
-
-            //act
-            var result = await _gameService.DeleteAsync(idTest);
-
-            //assert
-            result.Should().BeTrue();
-        }
-        [Fact]
-        public async void DeleteAsyncTestFailed()
-        {
-            //arrange
-            var idTest = -1;
-
-            //act
-            var result = await _gameService.DeleteAsync(idTest);
-
-            //assert
-            result.Should().BeFalse();
-        }
-
-        [Fact]
-        public async void UpdateAsyncTestSuccess()
-        {
-            //arrange
-
-            //act
-            var result = await _gameService.UpdateAsync(_gameTest);
-
-            //assert
-            result.Should().BeTrue();
-        }
-        [Fact]
-        public async void UpdateAsyncTestFailed()
-        {
-            //arrange
-
-            //act
-            var result = await _gameService.UpdateAsync(null);
-
-            //assert
-            result.Should().BeFalse();
+            result.Should().NotBeNull();
+            result.Game.Should().NotBeNull();
+            result.Game.Name.Should().BeNull();
         }
     }
 }
