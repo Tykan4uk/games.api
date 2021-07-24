caaa6dad97183949b6960f6430662f5d6abac359
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 23:29:08 +0300

    Adding MockService

---
+++ GamesApi/tests/GamesApi.UnitTests/Mocks/MockService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using GamesApi.Data;
+using GamesApi.Services;
+using GamesApi.Services.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace GamesApi.UnitTests.Mocks
+{
+    public class MockService : BaseDataService<GamesDbContext>
+    {
+        public MockService(
+            IDbContextWrapper<GamesDbContext> dbContextWrapper,
+            ILogger<MockService> logger)
+            : base(dbContextWrapper, logger)
+        {
+        }
+
+        public async Task RunWithException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                throw new Exception();
+            });
+        }
+
+        public async Task RunWithoutException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                return Task.FromResult(true);
+            });
+        }
+    }
+}
