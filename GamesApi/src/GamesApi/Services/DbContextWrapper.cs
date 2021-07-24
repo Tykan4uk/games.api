e7b34cfe055ac57f24261af4f4162f0c163f22d2
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 23:27:31 +0300

    Adding DbContextWrapper

---
+++ GamesApi/src/GamesApi/Services/DbContextWrapper.cs
@@ -0,0 +1,25 @@
+﻿using GamesApi.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace GamesApi.Services
+{
+    public class DbContextWrapper<T> : IDbContextWrapper<T>
+        where T : DbContext
+    {
+        private readonly T _dbContext;
+
+        public DbContextWrapper(
+            IDbContextFactory<T> dbContextFactory)
+        {
+            _dbContext = dbContextFactory.CreateDbContext();
+        }
+
+        public T DbContext => _dbContext;
+
+        public IDbContextTransaction BeginTransaction()
+        {
+            return _dbContext.Database.BeginTransaction();
+        }
+    }
+}
