be98a3a7ab10a87a89937c1835b72654c79f2e49
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 15:02:56 +0300

    AddRequest: adding properties ImageUrl and Description

--- GamesApi/src/GamesApi/Models/Requests/AddRequest.cs
+++ GamesApi/src/GamesApi/Models/Requests/AddRequest.cs
@@ -10,5 +10,7 @@ namespace GamesApi.Models.Requests
         public string Genre { get; set; }
         public DateTime ReleaseDate { get; set; }
         public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
